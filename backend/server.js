63919130a584000d84a65578580271f3b998c6a4
KrisanaWork <krisana.skwn@gmail.com>
2025-08-01 16:56:43 +0700

    update cors

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,9 @@ const { Pool } = require('pg');
 const app = express();
 const port = process.env.PORT || 3001;
 
-app.use(cors());
+app.use(cors({
+  origin: 'https://krisanawork.github.io'
+}));
 
 const pool = new Pool({
   host: process.env.DB_HOST,
